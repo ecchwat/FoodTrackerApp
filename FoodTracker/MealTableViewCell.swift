1dedd9f78740fd4494ba3938f0efb692a4ae4bb0
Elena Wiener <ecc.wiener@gmail.com>
2016-09-20 10:01:36 -0400

    FoodTracker2
    Second Food Tracker

--- FoodTracker/MealTableViewCell.swift
+++ FoodTracker/MealTableViewCell.swift
@@ -2,29 +2,31 @@
 //  MealTableViewCell.swift
 //  FoodTracker
 //
-//  Created by Jane Appleseed on 5/27/15.
-//  Copyright © 2015 Apple Inc. All rights reserved.
-//  See LICENSE.txt for this sample’s licensing information.
+//  Created by Elena Wiener on 8/4/16.
+//  Copyright © 2016 Elena. All rights reserved.
 //
+
 
 import UIKit
 
 class MealTableViewCell: UITableViewCell {
     // MARK: Properties
-
-    @IBOutlet weak var nameLabel: UILabel!
+    
+    //    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
+    
+    
 }
